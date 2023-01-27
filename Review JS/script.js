d5bf9e3050981d6096ef8791350ab9b41f81b75d
Albert Lei <albert.lei@hotmail.com>
2023-01-28 01:50:26 +0800

    initial moz-todo-react

--- Review JS/script.js
+++
@@ -1,9 +0,0 @@
-function createParagraph(params) {
-  let para = document.createElement("p");
-  para.textContent = "You clicked on this button";
-  document.body.appendChild(para);
-}
-const buttons = document.querySelectorAll("button");
-for (let i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", createParagraph);
-}
